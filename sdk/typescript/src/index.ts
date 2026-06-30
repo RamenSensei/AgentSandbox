67ec219fda3c3da9416eba520491019cf3adbbf1
Hangjen Chan <hangjenchan@users.noreply.github.com>
2026-06-30 20:23:01 +0800

    feat(sdk-ts): introduce index

---
+++ sdk/typescript/src/index.ts
@@ -0,0 +1,9 @@
+export * from "./types.js";
+export { KernelError, DenialError, TransportError } from "./errors.js";
+export {
+  Kernel,
+  EpisodeHandle,
+  BranchHandle,
+  EffectHandle,
+  type KernelOptions,
+} from "./client.js";
